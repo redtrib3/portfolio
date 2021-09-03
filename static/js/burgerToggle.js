df490fad1f5c589ea37b83a2826d29dd6c5bdab3
Anirudh <68897241+0xRedTribe@users.noreply.github.com>
2021-09-03 19:49:19 +0400

    Create burgerToggle.js

---
+++ static/js/burgerToggle.js
@@ -0,0 +1,14 @@
+// Script to Toggle Burger Menu (Mobile)
+
+const burgerIcon = document.querySelector("#burger");
+const navlinks = document.querySelector("#links_Nav");
+
+
+// event listener on Menu click 
+
+burgerIcon.addEventListener('click',() => {
+	burgerIcon.classList.toggle('is-active');   // Changes the Three bars to cross-symbol(X) on dropdown
+ 	navlinks.classList.toggle('is-active');   // Brings the Dropdown 
+})
+
+//EOF
